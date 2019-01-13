fe49878a334724860611255397ca4d1dd7304fdd
simodecl <simodecl@student.arteveldehs.be>
2019-01-13 15:30:23 +0100

    upload to gh-pages branch

--- README.md
+++
@@ -1,1 +0,0 @@
-# cmsdev-napp
--- angular/src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  tokenURL: 'https://simodecl.cmsdevelopment.be/wp-json/jwt-auth/v1/token',
-  apiURL: 'https://simodecl.cmsdevelopment.be/wp-json/wp/v2',
-  acfURL: 'https://simodecl.cmsdevelopment.be/wp-json/acf/v3'
-};
